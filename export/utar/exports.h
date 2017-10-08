5a9f891d0f6c33ae9bdeb2ad98f2aa867ce69ee0
twiddle-bits <twiddlebits.ak@gmail.com>
2017-10-08 02:16:17 -0400

    Base

---
+++ export/utar/exports.h
@@ -0,0 +1,31 @@
+/*! exports.h */
+
+#ifndef _UTAR_VERSION_H_
+#define _UTAR_VERSION_H_
+
+#if defined(_WIN32) && !defined(__GCC__)
+#  ifdef BUILDING_UTAR
+#    define UTAR_API __declspec(dllexport)
+#  else
+#    define UTAR_API __declspec(dllimport)
+#  endif
+#  ifndef _CRT_SECURE_NO_WARNINGS
+#    define _CRT_SECURE_NO_WARNINGS
+#  endif
+#else
+#  ifdef BUILDING_UTAR
+#    define UTAR_API __attribute__ ((visibility ("default")))
+#  else
+#    define UTAR_API 
+#  endif
+#endif
+
+#if defined(__cplusplus)
+#  define UTAR_EXTERN_C extern "C"
+#  define UTAR_C_API UTAR_EXTERN_C UTAR_API
+#else
+#  define UTAR_EXTERN_C
+#  define UTAR_C_API UTAR_API
+#endif
+
+#endif/*_UTAR_VERSION_H_*/
